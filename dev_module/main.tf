d57b7126a5a07f51fa8729994fe3deb67a5b6e41
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-02 18:45:45 +0530

    gh updated

--- dev_module/main.tf
+++ dev_module/main.tf
@@ -1,5 +1,5 @@
 module "custom_vpc" {
-  source     = "./modules/"
+  source     = "https://github.com/devsecopsmurali/CICD_terraform/tree/custom_modules/modules"
   cidr_value = "10.0.0.0/16"
   tenancy    = "default"
   region     = "us-east-1"

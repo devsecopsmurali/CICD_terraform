bae4d9aee830338f5626fec1447d42616ffaf824
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-03 17:48:59 +0530

    sdf

--- dev_module_2/main.tf
+++ dev_module_2/main.tf
@@ -8,10 +8,13 @@ resource "aws_instance" "web_servers" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro" 
 }
-# VPC
 output "ec2_id_test" {
-  value = "${aws_instance.web_servers.id}"
+  value = [for instance in aws_instance.web_servers.id : instance.public_ip]
 }
+
+# output "ec2_id_test" {
+#   value = "${aws_instance.web_servers.id}"
+# }
 
 provider "aws" {
   region     = "us-east-1"

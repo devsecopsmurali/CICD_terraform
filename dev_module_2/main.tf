8ad15491d3de3632e44d12c60dbd9c834359eeba
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-03 09:44:17 +0530

    asdfa

--- dev_module_2/main.tf
+++ dev_module_2/main.tf
@@ -2,10 +2,14 @@
 resource "aws_instance" "web_servers" {
 # Define the number of VMs
   count = 2
-
+  
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro" 
 }
 output "aws_instances" {
   value = [for instance in aws_instance.web_servers : instance.public_ip]
+}
+
+provider "aws" {
+  region     = "us-east-1"
 }

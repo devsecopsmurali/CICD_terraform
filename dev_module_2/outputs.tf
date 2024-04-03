12243856ab11482b6322f83438f882f09f4024e7
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-03 18:57:22 +0530

    sdf

--- dev_module_2/outputs.tf
+++ dev_module_2/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_id_test" {
-  value = "${aws_instance.web_servers.public_ip}"
+  value = aws_instance.web_servers.public_ip
   #value = [for instance in aws_instance.web_servers : instance.public_ip]
 }

c35dd8b7907bbaa1a623e9834213009cf9a43502
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-04 21:41:27 +0530

    sdf

--- dev_module_3/main.tf
+++ dev_module_3/main.tf
@@ -14,6 +14,11 @@ module "ec2_instance" {
     Environment = "dev"
   }
 }
+
+output "ec2_complete_public_ip" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = module.ec2_instance.public_ip
+}
 provider "aws" {
   region     = "us-east-1"
 }
